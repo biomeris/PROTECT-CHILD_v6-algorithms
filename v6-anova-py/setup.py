0e82b7849e172d161d06ff4c1fbd7c3655a17a43
Vittoria Ramella <vittoria.ramella@biomeris.it>
2026-02-19 16:54:08 +0100

    Added libraries requirements and updated algorithm_store.json

--- v6-anova-py/setup.py
+++ v6-anova-py/setup.py
@@ -5,24 +5,19 @@ from setuptools import setup, find_packages
 # we're using a README.md, if you do not have this in your folder, simply
 # replace this with a string.
 here = path.abspath(path.dirname(__file__))
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 # Here you specify the meta-data of your package. The `name` argument is
 # needed in some other steps.
 setup(
-    name='v6-anova-py',
+    name="v6-anova-py",
     version="1.0.0",
-    description='Federated ANOVA for distributed hypothesis testing, enabling variance analysis across multiple organizations without sharing raw data',
+    description="Federated ANOVA for distributed hypothesis testing, enabling variance analysis across multiple organizations without sharing raw data",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    # TODO add a url to your github repository here (or remove this line if
-    # you do not want to make your source code public)
-    # url='https://github.com/....',
+    long_description_content_type="text/markdown",
+    url="https://github.com/biomeris/PROTECT-CHILD_v6-algorithms/tree/main/v6-anova-py",
     packages=find_packages(),
-    python_requires='>=3.10',
-    install_requires=[
-        'vantage6-algorithm-tools',
-        'pandas'
-    ]
+    python_requires=">=3.10",
+    install_requires=["vantage6-algorithm-tools==4.9.1", "pandas", "numpy", "scipy"],
 )
